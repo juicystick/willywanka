830a8340d727e4a06069c7f38b206a187d916496
juicystick <153515671+CiccoSOL@users.noreply.github.com>
2024-01-25 20:40:13 +0100

    Cookie Class

---
+++ Cookie.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cookie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cookie extends Actor
+{
+    /**
+     * Act - do whatever the Cookie wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(this)==true) {
+            WankaWorld wankaWorld = (WankaWorld) getWorld();
+            wankaWorld.currentScore++;
+        };
+    }
+}
